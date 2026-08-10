da1509446e61052dda638df78942fd3a78ccaa09
aegonviserys-glitch <aegonviserys@gmail.com>
2026-08-11 00:45:59 +0700

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'mahjong-table-score-v14';
+const CACHE = 'mahjong-table-score-v16';
 const CDN = [
   'https://cdnjs.cloudflare.com/ajax/libs/react/18.2.0/umd/react.production.min.js',
   'https://cdnjs.cloudflare.com/ajax/libs/react-dom/18.2.0/umd/react-dom.production.min.js',
